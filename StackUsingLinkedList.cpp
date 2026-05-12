89733db71e81378177ab0db8be24d573a993dd45
Hugo140804 <hgugi3nsn@gmail.com>
2026-05-12 10:38:11 +0700

    Membuat Void peek

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -47,3 +47,23 @@ public:
         top = top->next;
         cout << "Popped value : " << top->data << endl;
     }
+
+    //
+    void peek()
+    {
+        if (top == NULL)
+        {
+            cout << "List is Empty" << endl;
+        }
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
+    }
+};
